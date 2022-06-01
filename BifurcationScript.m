7bd5c6e951421a5d46b646e914eb282ffc06db0a
Kaizad Wadia <kkw21@skcg-146b-019.ae.ic.ac.uk>
2022-06-01 20:27:44 +0100

    Bifurcation model implemented

--- BifurcationScript.m
+++ BifurcationScript.m
@@ -1,8 +1,8 @@
 addpath("Algorithms/", "Models/");
 
-params = ["L1" "R1" "be1" "L2" "R2" "be2" "L3" "R3" "be3" "RW1" "RW2" "Cwk"];
-xmin = [0.005,0,0,0.005,0,0,0.005,0,0,0,0,0];
-xmax = [0.3,0.02,0.0025,0.3,0.02,0.0025,0.3,0.02,0.0025,740000000,5.22e+09,1e-7,];
+params = ["L1" "R1" "be1" "L2" "R2" "be2" "RW1" "RW2" "Cwk"];
+xmin = [0.005,0,0,0.005,0,0,0,0,0];
+xmax = [0.3,0.02,0.0025,0.3,0.02,0.0025,740000000,5.22e+09,1e-7,];
 scaler = MinMaxScaler(xmin, xmax);
 
 problem = OptimisationProblem(BifurcationModel, params, scaler);
@@ -14,7 +14,7 @@ switch (measurements)
         problem.optimiser = NelderMeadSimplex;
         problem.optimiser.x0Tol = 5;
         problem.optimiser.lenX = length(params);
-        problem.optimiser.epochs = 20;
+        problem.optimiser.epochs = 30;
         
         [xpred, errP, erract, nguesses] = problem.fitsolution(0)
     case (1)
