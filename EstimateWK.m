d0b0eed06da35dab71bd07d7cc66a0d42473789d
Kaizad Wadia <kkw21@skcg-146b-019.ae.ic.ac.uk>
2022-06-28 18:36:58 +0100

    Found Windkessel from impedence

--- EstimateWK.m
+++ EstimateWK.m
@@ -1,16 +1,19 @@
 addpath("Algorithms/");
 
-Z = Z5;
+Z = P11out ./ Q11out;
 om = omegas;
 
-zfun = @(x, om) (x(1) + x(2) - 1i .* om .* x(1) .* x(2) .* x(3))...
+zfun = @(x) (x(1) + x(2) - 1i .* om .* x(1) .* x(2) .* x(3))...
     ./ (1 - 1i .* om .* x(2) .* x(3)); % x = [RW1, RW2, Cwk]
 
 
 opt = NelderMeadSimplex;
-scaler = MinMaxScaler([0, 0, 0], [740000000,5.22e+09,1e-7]);
+scaler = MinMaxScaler([0, 0, 0], [1e11,1e11,1e-7]);
 
-fun = @(xp) mean(abs(zfun(scaler.inv_transform(xp), om) - Z).^2);
+xact = [RW1(11), RW2(11), CWK(11)];
+
+fun = @(xp) mean(abs(zfun(scaler.inv_transform(xp)) - Z).^2);
 opt.lenX = 3;
-opt.x0Tol = 1e9;
-[xpred, nguesses] = opt.fit(fun, [0.5, 0.5, 0.5]);
+opt.x0Tol = 5e16;
+xpred = opt.fit(fun, 0);
+Perr = 100.*opt.relerr(scaler.inv_transform(xpred), xact)
