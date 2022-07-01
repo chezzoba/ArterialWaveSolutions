8687c93b4d128b130a06d65c9716e0c10f9ca8ed
Kaizad Wadia <kkw21@skcg-146b-019.ae.ic.ac.uk>
2022-07-01 18:23:13 +0100

    Estimation of Windkessel Parameters

--- EstimateWK.m
+++ EstimateWK.m
@@ -7,8 +7,22 @@ CWKs = [0,0,0,0,0,0,0,0,0,0,8.6974,1.767,7.0871,12.1836,16.7453,17.1017,17.1017,
 flows = load('PreviousCode/automeris/aorta/bc_outlet_11_flow.csv');
 pressures = load('PreviousCode/automeris/aorta/bc_outlet_11_Pressure.csv');
 
-xpred = WindKessels(pressures, flows)
+Z = P7out ./ Q7out;
+om = omegas;
 
-xact = [RW1s(11), RW2s(11), CWKs(11)]
+zfun = @(x) (x(1) + x(2) - 1i .* om .* x(1) .* x(2) .* x(3))...
+    ./ (1 - 1i .* om .* x(2) .* x(3)); % x = [RW1, RW2, Cwk]
 
-Perr = 100.*ConOptimisation.relerr(xpred, xact)
+
+opt = ConOptimisation;
+opt.plt = false;
+scaler = MinMaxScaler([1e7, 1e8, 0], [1e9,1e10,1e-8]);
+
+fun = @(xp) mean(abs(zfun(scaler.inv_transform(xp)) - Z).^2);
+opt.lenX = 3;
+opt.x0Tol = 1e30;
+xpred = scaler.inv_transform(opt.fit(fun, 0))
+
+% xact = [RW1s(11), RW2s(11), CWKs(11)]
+
+% Perr = 100.*ConOptimisation.relerr(xpred, xact)
